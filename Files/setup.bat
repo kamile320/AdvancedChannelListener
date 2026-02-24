a40a421baf01eb5ca03abb92c9aa3e12e870ca1d
Krzysztof M <k.marszalowicz@gmail.com>
2026-02-24 10:45:36 +0100

    Moved to Files/ and modified to only install pip libraries

---
+++ Files/setup.bat
@@ -0,0 +1,12 @@
+@echo off
+@echo ACL module auto-setup
+@echo This .bat file will download all pip libraries for Python script
+@echo Make sure you have downloaded Python 3 with 'pip'
+
+pause
+
+pip install -r requirements.txt
+
+@echo Done.
+
+pause
